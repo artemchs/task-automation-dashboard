7e5555f8507cce4f54ae4b19b8923e710405699b
Artem Chernysh <artemchs123@gmail.com>
2024-12-23 11:29:46 +0100

    feat(client): :globe_with_meridians: set up react-router

--- apps/client/src/main.jsx
+++ apps/client/src/main.jsx
@@ -1,8 +1,19 @@
+// React
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 
+// React Router
+import { BrowserRouter, Routes, Route } from 'react-router'
+
+// Pages
+import { Home } from './pages/Home'
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <>Hello</>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   </StrictMode>
 )
